e9e10a2d25bab0cd8139e60a2dee86d2cf3a2e24
malhotran-math <malhotra.nandan1409@gmail.com>
2025-08-25 14:13:50 +0200

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -1,3 +1,16 @@
 # Extract trunk diameters information and sort from smallest to largest
 sorted_trunk_diameters = np.sort(tree_census[:,2])
 print(sorted_trunk_diameters)
+
+
+# Create an array which contains row data on the largest tree in tree_census
+largest_tree_data = tree_census[tree_census[:, 2] == 51]
+print(largest_tree_data)
+
+# Slice largest_tree_data to get only the block ID
+largest_tree_block_id = largest_tree_data[:, 1]
+print(largest_tree_block_id)
+
+# Create an array which contains row data on all trees with largest_tree_block_id
+trees_on_largest_tree_block = tree_census[tree_census[:,1]==largest_tree_block_id]
+print(trees_on_largest_tree_block)
